6ea8abb80122578522ba3fe35f7feab180c67b2c
ChenTongfei <872245384@qq.com>
2019-06-08 23:03:53 +0800

    提交v1中文件 lycsb.cpp 和 p_allocator.cpp

--- Programming-FPTree/src/lycsb.cpp
+++ Programming-FPTree/src/lycsb.cpp
@@ -5,21 +5,27 @@
 #define VALUE_LEN 8
 using namespace std;
 
-const string workload = "";
+const string workload = "/home/chen/桌面/DB/2019-DBMS-Project/Programming-FPTree/workloads/";
 
-const string load = workload + ""; // TODO: the workload_load filename
-const string run  = workload + ""; // TODO: the workload_run filename
+const string load = workload + "1w-rw-50-50-load.txt"; // TODO: the workload_load filename
+const string run  = workload + "1w-rw-50-50-run.txt"; // TODO: the workload_run filename
 
 const string filePath = "";
 
-const int READ_WRITE_NUM = 0; // TODO: how many operations
+const int READ_WRITE_NUM = 2200000; // TODO: how many operations
 
 int main()
 {        
     leveldb::DB* db;
     leveldb::Options options;
     leveldb::WriteOptions write_options;
+    leveldb::ReadOptions read_options;
     // TODO: open and initial the levelDB
+
+    options.create_if_missing = true;
+    leveldb::Status status = leveldb::DB::Open(options,"/tmp/testdb", &db);
+    assert(status.ok());
+
     uint64_t inserted = 0, queried = 0, t = 0;
     uint64_t* key = new uint64_t[2200000]; // the key and value are same
     bool* ifInsert = new bool[2200000]; // the operation is insertion or not
@@ -31,9 +37,37 @@ int main()
 
     printf("Load phase begins \n");
     // TODO: read the ycsb_load and store
+
+    ycsb_load = fopen(load.c_str(),"r");
+    assert(ycsb_load);
+    int len = 2200000;
+    buf = new char[64];
+    uint64_t  tmp;
+    for(int i = 0;i < len;i++)    {
+        if(fscanf(test,"%s%lu",buf,&tmp) != EOF)     {
+            key[i] = tmp;
+            if(*buf == 'I')
+                ifInsert[i] = true;
+            else   ifInsert[i] = false;
+        }else
+        {
+            break;
+        }
+        
+    }
+    fclose(ycsb_load);
+
+
     clock_gettime(CLOCK_MONOTONIC, &start);
 
     // TODO: load the workload in LevelDB
+
+    for (int i = 0; i < READ_WRITE_NUM; i++) {
+        if (ifInsert[i]) {
+            status = db->Put(write_options, to_string(key[i]), to_string(key[i]));
+            inserted++;
+        }
+    }
 
     clock_gettime(CLOCK_MONOTONIC, &finish);
 	single_time = (finish.tv_sec - start.tv_sec) * 1000000000.0 + (finish.tv_nsec - start.tv_nsec);
@@ -47,9 +81,40 @@ int main()
 
     // TODO:read the ycsb_run and store
 
+    ycsb_run = fopen(run.c_str(),"r");
+    assert(ycsb_run);
+
+    buf = new char[64];
+    uint64_t  tmp;
+    for(int i = 0;i < READ_WRITE_NUM;i++)    {
+        if(fscanf(test,"%s%lu",buf,&tmp) != EOF)     {
+            key[i] = tmp;
+            if(*buf == 'I')
+                ifInsert[i] = true;
+            else   ifInsert[i] = false;
+        }else
+        {
+            break;
+        }
+        
+    }
+    fclose(ycsb_load);
+
     clock_gettime(CLOCK_MONOTONIC, &start);
 
     // TODO: operate the levelDB
+
+     for (int i = 0; i < READ_WRITE_NUM; i++) {
+        if (ifInsert[i]) {
+            db->Put(write_options, to_string(key[i]), to_string(key[i]));
+            inserted++;
+        }else{
+            string value;
+            db->Get(read_options, to_string(key[i]), &value);
+            queried++;
+        }
+    }
+
 
 	clock_gettime(CLOCK_MONOTONIC, &finish);
 	single_time = (finish.tv_sec - start.tv_sec) + (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
--- Programming-FPTree/src/p_allocator.cpp
+++ Programming-FPTree/src/p_allocator.cpp
@@ -31,59 +31,164 @@ PAllocator::PAllocator() {
     if (allocatorCatalog.is_open() && freeListFile.is_open()) {
         // exist
         // TODO
+
+        allocatorCatalog.read((char *)(&maxFileId), sizeof(maxFileId));
+        allocatorCatalog.read((char *)(&freeNum), sizeof(freeNum));
+        allocatorCatalog.read((char *)(&startLeaf), sizeof(startLeaf));
+        allocatorCatalog.close();
+        for(int i =0;i < freeNum;i++)  {
+            PPointer tmp;
+            freeListFile.read( (char*) (&tmp),sizeof(tmp) );
+            freeList.push_back(tmp);
+        }
+        freeListFile.close();
     } else {
         // not exist, create catalog and free_list file, then open.
         // TODO
+        ofstream catalog(allocatorCatalogPath, ios::out);
+        ofstream freelist(freeListPath, ios::out);
+        catalog.close();
+        freelist.close();
+       fopen(allocatorCatalogPath , "wb");
+       fclose(allocatorCatalogPath);
+       maxFileId = 1;
+        freeNum = 0;
     }
     this->initFilePmemAddr();
 }
 
 PAllocator::~PAllocator() {
     // TODO
+    persistCatalog();
+    string tmppath = DATA_DIR + P_ALLOCATOR_FREE_LIST;
+    ofstream freeListFile(tmppath,ios::out|ios::binary);
+    //open file
+    uint16_t i;
+    //xuanhuan
+    for(i = 0;i < freeNum;i++)  {
+        PPointer tow = freeList[i];
+        //write
+        freeListFile.write((char *)(&tow), sizeof(tow)); 
+    }
+    //close
+    freeListFile.close();
+    pAllocator = NULL;
+    //finish
 }
 
 // memory map all leaves to pmem address, storing them in the fId2PmAddr
 void PAllocator::initFilePmemAddr() {
     // TODO
+    uint64_t i;
+    size_t mapped_len;
+    int is_pmem;
+    size_t leafgroup_len = LEAF_GROUP_HEAD+LEAF_GROUP_AMOUNT*calLeafSize();
+    for(i = 1;i < maxFileId;i++)    {
+        string filepath = DATA_DIR + to_string(i);
+        
+        fId2PmAddr[i] = (char*)pmem_map_file(filepath.c_str(), leafgroup_len, PMEM_FILE_CREATE,
+                                      0666, &mapped_len, &is_pmem); 
+
+    }   
+
+    
 }
 
 // get the pmem address of the target PPointer from the map fId2PmAddr
 char* PAllocator::getLeafPmemAddr(PPointer p) {
     // TODO
-    return NULL;
+    if(p.fileId <= maxFileId && p.fileId != ILLEGAL_FILE_ID)    {
+        return fId2PmAddr[p.fileId] + p.offset;
+    }
+    else
+        return NULL;
 }
 
 // get and use a leaf for the fptree leaf allocation
 // return 
 bool PAllocator::getLeaf(PPointer &p, char* &pmem_addr) {
     // TODO
-    return false;
+    if(freeList.empty())    {
+        newLeafGroup();
+    }
+    p = freeList.back();
+    freeList.pop_back();
+    freeNum--;
+    pmem_addr = getLeafPmemAddr(p);
+    
+    string path = DATA_DIR + to_string(p.fileId);
+    fstream LeafGroupfile(path,ios::in|ios::out|ios::binary);
+    uint64_t usedtmie;
+    uint8_t bitmap[LEAF_GROUP_AMOUNT];
+    LeafGroupfile.read((char*)&bitmap,sizeof(bitmap));
+     bitmap[(p.offset-LEAF_GROUP_HEAD)/calLeafSize()] = 1;
+    LeafGroupfile.read((char*)&usedtmie,sizeof(usedtmie));
+    usedtmie++;
+    LeafGroupfile.seekg(0,ios::beg);
+    LeafGroupfile.write((char*)&usedtmie,sizeof(usedtmie));
+    LeafGroupfile.write((char*)&bitmap,sizeof(bitmap));
+    LeafGroupfile.close();
+    return true;
+
 }
 
 bool PAllocator::ifLeafUsed(PPointer p) {
     // TODO
+    if(ifLeafExist(p))  {
+        for(int i = 0;i < freeList.size();i++)  {
+            if(p == freeList[i])
+                return false;
+        }
+        return true;
+    }
     return false;
 }
 
 bool PAllocator::ifLeafFree(PPointer p) {
     // TODO
+    if(ifLeafExist(p) && !ifLeafUsed(p))    
+        return true;
     return false;
 }
 
 // judge whether the leaf with specific PPointer exists. 
 bool PAllocator::ifLeafExist(PPointer p) {
     // TODO
+    if (p.fileId < maxFileId && p.fileId != ILLEGAL_FILE_ID)
+        return true;
+    return false;
 }
 
 // free and reuse a leaf
 bool PAllocator::freeLeaf(PPointer p) {
     // TODO
-    return false;
+    Byte bit = 0;
+    uint64_t usedtime;
+    string path = DATA_DIR + to_string(p.fileId);
+    fstream file(path,ios::in|ios::binary|ios::out);
+    if(!file.good())    return false;
+    file.read((char*)&(usedtime), sizeof(uint64_t));
+    usedtime--;
+
+    file.seekg(0,ios::beg);
+    file.write((char*)&(usedtime), sizeof(uint64_t));
+    file.seekg((sizeof(uint64_t) + ((p.offset - LEAF_GROUP_HEAD) / calLeafSize()), ios::beg));
+    file.write((char*)&(bit), sizeof(Byte));
+    freeList.push_back(p);
+    freeNum++;
+    return true;
 }
 
 bool PAllocator::persistCatalog() {
     // TODO
-    return false;
+    string allocatorCatalogPath = DATA_DIR + P_ALLOCATOR_CATALOG_NAME;
+    ofstream allocatorCatalog(allocatorCatalogPath, ios::out|ios::binary);
+    if (!allocatorCatalog)
+        return false;
+    allocatorCatalog.write((char *)(&maxFileId), sizeof(maxFileId));
+    allocatorCatalog.write((char *)(&freeNum), sizeof(freeNum));
+    allocatorCatalog.write((char *)(&startLeaf), sizeof(startLeaf));
+    return true;
 }
 
 /*
@@ -93,5 +198,25 @@ bool PAllocator::persistCatalog() {
 // create a new leafgroup, one file per leafgroup
 bool PAllocator::newLeafGroup() {
     // TODO
+string fileIdPath=DATA_DIR + to_string(maxFileId);
+    ofstream leafGroup(fileIdPath,ios::out|ios::binary);
+    if(leafGroup.is_open())
+    {
+        uint64_t usedNum = 0;
+        uint8_t bitmap[LEAF_GROUP_AMOUNT*(1+calLeafSize())] = {0};        
+        leafGroup.write((char*)&usedNum,sizeof(usedNum));
+        leafGroup.write((char*)&bitmap,sizeof(bitmap));
+        int i = 0;
+        for (i = 0; i < LEAF_GROUP_AMOUNT; i++) {
+            PPointer p;
+            p.fileId = maxFileId;
+            p.offset = LEAF_GROUP_HEAD + i * calLeafSize();
+            freeList.push_back(p);
+        } 
+        freeNum += LEAF_GROUP_AMOUNT;   
+        maxFileId++;    
+        leafGroup.close();
+        return true;
+    }
     return false;
 }
